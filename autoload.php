a13b82ad6038eafb648ea1f98ad8517b40fcb2c3
Ammar Faizi <ammarfaizi2@gmail.com>
2019-11-09 20:21:40 +0700

    Create autoloader

--- autoload.php
+++ autoload.php
@@ -1,3 +1,14 @@
 <?php
 
-require __DIR__."/src/classes/PhpPreprocessor/PhpPreprocessor.php";
+/**
+ * @param string $class
+ * @return void
+ */
+function phpPreprocessorAutoloader(string $class)
+{
+	if (file_exists($f = __DIR__."/src/classes/PhpPreprocessor/".str_replace("\\", "/", $class).".php")) {
+		require $f;
+	}
+}
+
+spl_autoload_register("phpPreprocessorAutoloader");
